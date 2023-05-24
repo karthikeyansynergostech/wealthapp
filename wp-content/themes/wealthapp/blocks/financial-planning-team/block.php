94ce93796c2dee41d734c2bc92aa3b982e9269e6
karthikeyanbmca <karthikeyanbmca@gmail.com>
2023-05-24 23:39:18 +0530

    24-may-2023-aboutus eod

---
+++ wp-content/themes/wealthapp/blocks/financial-planning-team/block.php
@@ -0,0 +1,355 @@
+<div id="financial-planning-team-thumbnail" class="key-management-thumbnail team-slider-thumbnail owl-carousel owl-theme" data-child=".financial-planning-team">
+	<?php if(block_value('s1-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s1-profilepic'),"Full size");
+	?>
+	<div class="item">
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s1-profilepic");?>" alt="<?php block_value("s1-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s1-name");?></p>
+					<p class='designation'><?php echo block_value("s1-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s2-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s2-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s2-profilepic");?>" alt="<?php block_value("s2-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s2-name");?></p>
+					<p class='designation'><?php echo block_value("s2-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s3-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s3-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s3-profilepic");?>" alt="<?php block_value("s3-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s3-name");?></p>
+					<p class='designation'><?php echo block_value("s3-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s4-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s4-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s4-profilepic");?>" alt="<?php block_value("s4-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s4-name");?></p>
+					<p class='designation'><?php echo block_value("s4-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s5-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s5-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s5-profilepic");?>" alt="<?php block_value("s5-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s5-name");?></p>
+					<p class='designation'><?php echo block_value("s5-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s6-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s6-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s6-profilepic");?>" alt="<?php block_value("s6-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s6-name");?></p>
+					<p class='designation'><?php echo block_value("s6-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s7-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s7-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s7-profilepic");?>" alt="<?php block_value("s7-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s7-name");?></p>
+					<p class='designation'><?php echo block_value("s7-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s8-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s8-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s8-profilepic");?>" alt="<?php block_value("s8-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s8-name");?></p>
+					<p class='designation'><?php echo block_value("s8-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s9-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s9-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s9-profilepic");?>" alt="<?php block_value("s9-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s9-name");?></p>
+					<p class='designation'><?php echo block_value("s9-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s10-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s10-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<a href="javascript:void(0)" class="goto" >
+			<div class="item-content">
+				<div class="image-block">
+					<img src="<?php echo block_field("s10-profilepic");?>" alt="<?php block_value("s10-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+				</div>
+				<div class="info-block">
+					<p class='name'><?php echo block_value("s10-name");?></p>
+					<p class='designation'><?php echo block_value("s10-designation");?></p>
+				</div>
+			</div>
+		</a>
+	</div>
+	<?php } ?>
+
+
+</div>
+
+<div id="financial-planning-team" class="key-management team-slider owl-carousel owl-theme" data-child=".financial-planning-team-thumbnail">
+	<?php if(block_value('s1-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s1-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s1-profilepic");?>" alt="<?php block_value("s1-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s1-name");?></p>
+				<p class='designation'><?php echo block_value("s1-designation");?></p>
+				<?php echo block_value("s1-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s2-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s2-profilepic'),"Full size");
+	?>
+	<div class="item">
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s2-profilepic");?>" alt="<?php block_value("s2-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s2-name");?></p>
+				<p class='designation'><?php echo block_value("s2-designation");?></p>
+				<?php echo block_value("s2-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s3-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s3-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s3-profilepic");?>" alt="<?php block_value("s3-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s3-name");?></p>
+				<p class='designation'><?php echo block_value("s3-designation");?></p>
+				<?php echo block_value("s3-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s4-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s4-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s4-profilepic");?>" alt="<?php block_value("s4-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s4-name");?></p>
+				<p class='designation'><?php echo block_value("s4-designation");?></p>
+				<?php echo block_value("s4-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s5-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s5-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s5-profilepic");?>" alt="<?php block_value("s5-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s5-name");?></p>
+				<p class='designation'><?php echo block_value("s5-designation");?></p>
+				<?php echo block_value("s5-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s6-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s6-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s6-profilepic");?>" alt="<?php block_value("s6-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s6-name");?></p>
+				<p class='designation'><?php echo block_value("s6-designation");?></p>
+				<?php echo block_value("s6-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s7-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s7-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s7-profilepic");?>" alt="<?php block_value("s7-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s7-name");?></p>
+				<p class='designation'><?php echo block_value("s7-designation");?></p>
+				<?php echo block_value("s7-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s8-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s8-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s8-profilepic");?>" alt="<?php block_value("s8-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s8-name");?></p>
+				<p class='designation'><?php echo block_value("s8-designation");?></p>
+				<?php echo block_value("s8-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+
+	<?php if(block_value('s9-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s9-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s9-profilepic");?>" alt="<?php block_value("s9-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s9-name");?></p>
+				<p class='designation'><?php echo block_value("s9-designation");?></p>
+				<?php echo block_value("s9-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+	
+	<?php if(block_value('s10-profilepic')!=0){
+	$image_attributes = wp_get_attachment_image_src($attachment_id = block_value('s10-profilepic'),"Full size");
+	?>
+	<div class="item"> 
+		<div class="item-content">
+			<div class="image-block">
+				<img src="<?php echo block_field("s10-profilepic");?>" alt="<?php block_value("s10-name");?>"  width="<?php echo $image_attributes[1];?>"   height="<?php echo $image_attributes[2];?>" />
+			</div>
+			<div class="info-block">
+				<p class='name'><?php echo block_value("s10-name");?></p>
+				<p class='designation'><?php echo block_value("s10-designation");?></p>
+				<?php echo block_value("s10-description");?>
+			</div>
+		</div>
+	</div>
+	<?php } ?>
+</div>
